e202ea53ba3d907ac01988465b6c7336cfdf73da
loveKino <842914439@qq.com>
2017-06-08 21:48:42 +0800

    opt

--- test/system/js/letbinding.js
+++ test/system/js/letbinding.js
@@ -4,12 +4,24 @@ let {
     equalJsApp,
 } = require('../../util');
 
+let insideFun = `let x = 1;
+let f = (y) -> {
+    let insideFun = (v, u) -> + (v, u);
+    insideFun(x, y);
+};
+
+let c = f(10);
+
++ (c, f(1));
+`;
+
 let testData = {
     // let binding
     'let x = 1; +(x, 1)': 2,
     'let x = 1, y = 2; + (x, y)': 3,
     'let x = 1; let z = + (x, 1); z': 2,
-    'let x = 1; let y = () -> + (x, 1); y()': 2
+    'let x = 1; let y = () -> + (x, 1); y()': 2,
+    [insideFun]: 13
 };
 
 describe('letbinding', () => {
