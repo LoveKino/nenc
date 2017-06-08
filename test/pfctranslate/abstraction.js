424bee8c6bec4116c79e52636c7116ac7cf89439
loveKino <842914439@qq.com>
2017-06-08 17:35:04 +0800

    support statement

--- test/pfctranslate/abstraction.js
+++ test/pfctranslate/abstraction.js
@@ -9,7 +9,12 @@ let testData = {
     '(a) -> 1': 'sys_statements(sys_exp(sys_abstraction(sys_variable("a"), sys_data(sys_number("1")))))',
     '(a, b) -> 1': 'sys_statements(sys_exp(sys_abstraction(sys_pair(sys_variable("a"), sys_variable("b")), sys_data(sys_number("1")))))',
     '(a, b, c) -> 1': 'sys_statements(sys_exp(sys_abstraction(sys_pair(sys_variable("a"), sys_pair(sys_variable("b"), sys_variable("c"))), sys_data(sys_number("1")))))',
-    '(a) -> (b) -> 1': 'sys_statements(sys_exp(sys_abstraction(sys_variable("a"), sys_abstraction(sys_variable("b"), sys_data(sys_number("1"))))))'
+    '(a) -> (b) -> 1': 'sys_statements(sys_exp(sys_abstraction(sys_variable("a"), sys_abstraction(sys_variable("b"), sys_data(sys_number("1"))))))',
+
+    '((a) -> + (a, 1))(1)': 'sys_statements(sys_exp(sys_application(sys_abstraction(sys_variable("a"), sys_application(sys_variable("+"), sys_pair(sys_variable("a"), sys_data(sys_number("1"))))), sys_data(sys_number("1")))))',
+
+    '(a) -> {4; 5}': 'sys_statements(sys_exp(sys_abstraction(sys_variable("a"), sys_statements(sys_pair(sys_exp(sys_data(sys_number("4"))), sys_exp(sys_data(sys_number("5"))))))))',
+    '((a) -> {+ (a, 1)})(1)': 'sys_statements(sys_exp(sys_application(sys_abstraction(sys_variable("a"), sys_statements(sys_exp(sys_application(sys_variable("+"), sys_pair(sys_variable("a"), sys_data(sys_number("1"))))))), sys_data(sys_number("1")))))'
 };
 
 describe('abstraction', () => {
