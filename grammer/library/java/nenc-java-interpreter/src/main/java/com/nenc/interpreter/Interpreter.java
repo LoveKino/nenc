596819f8b3483b6fa30a47f8dc72a587def26891
loveKino <842914439@qq.com>
2017-07-16 16:39:01 +0800

    opt

---
+++ grammer/library/java/nenc-java-interpreter/src/main/java/com/nenc/interpreter/Interpreter.java
@@ -0,0 +1,16 @@
+package com.nenc.interpreter;
+
+class Interpreter {
+    public Object runProgram(String modulePath) {
+        return "";
+    }
+
+    public void defineModule() {
+    }
+
+    public void addMetaMethod() {
+    }
+
+    public void addLazyMetaMethod() {
+    }
+}
